4dad7c7e6755761104d7516ba864bbda19deb2df
Bernardo da Eira Duarte <bernardoeiraduarte@gmail.com>
2018-01-22 22:42:51 -0200

    Removing warnings

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
+int main()
 {
 	printf("Hello World\n");
 	return 0;
--- tester.c
+++ tester.c
@@ -1,7 +1,7 @@
 
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
+int main()
 {
 	return 0;
 }
